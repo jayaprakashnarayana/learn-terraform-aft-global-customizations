ed4658fb8ed1d5c9412ce9ac6c90d9c48234cc46
Sahil Bansal <sahil2311@gmail.com>
2022-05-05 04:30:52 +0530

    added role

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -1,21 +1,15 @@
 data "aws_caller_identity" "aws_caller_identity" {}
 
-#resource "aws_iam_role" "test_role" {
-#  name = "${data.aws_caller_identity.aws_caller_identity.account_id}-admin-role"
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole"
-#        Effect = "Allow"
-#        Principal = {
-#          AWS = ["008298205562"]
-#        }
-#      },
-#    ]
-#  })
-#  managed_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
-#  tags = {
-#    tag-key = "${data.aws_caller_identity.aws_caller_identity.account_id}-admin-role"
-#  }
-#}
+resource "aws_iam_role" "admin_role" {
+  name = "admin-xa-role"
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Action    = "sts:AssumeRole",
+        Principal = { "AWS" : "arn:aws:iam::${data.aws_caller_identity.aws_caller_identity.account_id}:root" }
+      }]
+  })
+}
